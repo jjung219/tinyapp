cf2b4d4ad37f3dd0a8216d6f3baf9a6137d3c15c
Jennifer Jung <jenjung2194@gmail.com>
2020-11-09 21:44:10 +0000

    added basic HTML skeleton from compass and added <a> tag that goes to a link to shorten URLs - dead link for now

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
@@ -21,6 +18,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+app.get("/urls", (req,res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
