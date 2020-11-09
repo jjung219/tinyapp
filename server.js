6ed493dde5af9fa9dfb832bcd5ac594a367540c8
Jennifer Jung <jenjung2194@gmail.com>
2020-11-09 23:59:57 +0000

    added urls_new that has the form on url/new, added generateRandomString function in server.js after installing body-parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,6 +12,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+const generateRandomString = () => {
+  let randomString = Math.floor(Math.random() * 100000).toString(16);
+  while (randomString.length < 7) {
+    randomString += 'a';
+  }
+  return randomString;
+
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
@@ -23,7 +35,17 @@ app.get("/urls", (req,res) => {
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
-})
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
